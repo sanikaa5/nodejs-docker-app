9786261bcea27b23672fc953c0bf5db07dd3ccce
Sanika Dhakite <167352722+sanikaa5@users.noreply.github.com>
2025-08-01 00:23:42 +0530

    Update build_and_push.sh

--- myapp/scripts/build_and_push.sh
+++ myapp/scripts/build_and_push.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Building Docker image..."
+IMAGE="sanikaa5/node-app:$GIT_COMMIT"
+docker build -t $IMAGE .
+
+echo "Pushing to DockerHub..."
+docker push $IMAGE
 
