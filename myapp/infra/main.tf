781dcd920a7854a94e8ee3d3489e7f5d494f892f
Sanika Dhakite <167352722+sanikaa5@users.noreply.github.com>
2025-08-01 00:21:49 +0530

    Update main.tf

--- myapp/infra/main.tf
+++ myapp/infra/main.tf
@@ -1,1 +1,59 @@
+provider "aws" {
+  region     = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "web_sg" {
+  name   = "web_sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami                         = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 (ap-south-1)
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public.id
+  associate_public_ip_address = true
+  key_name                    = var.key_name
+  vpc_security_group_ids      = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "NodeAppInstance"
+  }
+}
+
+resource "aws_eip" "app_ip" {
+  instance = aws_instance.app_server.id
+}
 
